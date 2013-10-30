862d27eca75eab548d7eea1c4c6a412720262465
damian izco <damian.izco@globant.com>
2013-10-30 14:12:43 -0300

    update

--- main.js
+++ main.js
@@ -1,42 +1,58 @@
-var lasturl="";
 
 $(document).ready(function(){
+  // init date
+  var initDate = new Date();
+
+  // check if hash has date selected
+  if (window.location.hash.split("?")[1]) {
+    var day = window.location.hash.split("day=")[1].split("&")[0],
+      month = window.location.hash.split("month=")[1].split("&")[0] ,
+      year = window.location.hash.split("year=")[1].split("&")[0];
+
+    initDate = new Date(year,month-1,day);
+  }
+  // set date-picker
+  $("#date-ini").val( formatDate(initDate) );
 
   checkURL();
-  $('.navigation li a').click(function (e){
-    checkURL(this.hash);
+  $('.navigation li a').click( function (e) {
+    this.hash= checkURL(this.hash);
     $(".navigation").find(".selected").removeClass("selected");
-    $(this).addClass("selected");
-
+    $(this).parent().addClass("selected");
   });
- // setInterval("checkURL()", 250);
 
-  var today = new Date();
-  $("#date-ini").val(formatDate(today));
+  $("#date-ini").change( function (e) {
+    this.hash= checkURL("#historico");
+    $(".navigation").find(".selected").removeClass("selected");
+    $(".navigation li a").last().addClass("selected");
+  });
 
 });
 
 // formats date as yyyy-mm-dd for date input
 function formatDate(date){
-    var dd = date.getDate(),
-      mm = date.getMonth()+1, //January is 0!
-      yyyy = date.getFullYear();
-      
-    if(dd<10) dd='0'+dd;
-    if(mm<10) mm='0'+mm;
+  var dd = date.getDate(),
+    mm = date.getMonth()+1, //January is 0!
+    yyyy = date.getFullYear();
 
-    return  yyyy+'-'+mm+'-'+dd;
+  if(dd<10) dd='0'+dd;
+  if(mm<10) mm='0'+mm;
+
+  return  yyyy+'-'+mm+'-'+dd;
 }
 
 function checkURL(hash) {
   if(!hash) hash = window.location.hash;
-  if (hash==="") window.location.hash = "#mediciones";
+  if (hash==="") {
+    window.location.hash = "#mediciones";
+  } else if (hash.split("?")[0]==="#historico") {
+    var myDate=$("#date-ini").val().split("-"),
+        queryString= "?"+"day="+ myDate[2] +"&month="+ myDate[1] +"&year="+ myDate[0];
+    hash = "#historico" + queryString;
+  }
+  loadPage(hash);
 
-  //if(hash !== lasturl || hash==="#historico") {
-    lasturl=hash;
-    // FIX - if we've used the history buttons to return to the homepage,
-    loadPage(hash);
-  //}
+  return hash;
 }
 
 // content manager
@@ -52,27 +68,41 @@ function loadPage(hash)
 
 // given a hash, retruns a filename and data object
 function route(hash, callback) {
-  var file, url;
+  var file, url, queryString;
 
   // routing
-  if (hash === "#historico") {
+  if (hash.split("?")[0] === "#historico") {
     var myDate=$("#date-ini").val().split("-");
+    queryString= encodeURIComponent("day="+ myDate[2] +"&month="+ myDate[1] +"&year="+ myDate[0]);
 
-    file = "./pages/historico.html";
-    url = "http://air-monitor.appspot.com/resources/air-samples?" +
-          "day="+ myDate[2] +"&month="+ myDate[1] +"&year="+ myDate[0];
-  } else {
-    file = "./pages/mediciones.html";
-    url = "http://air-monitor.appspot.com/resources/air-sample";
+    file ="./pages/historico.html";
+    url ="historico";
+  } else if (hash==="#mediciones") {
+    file ="./pages/mediciones.html";
+    url ="mediciones";
+    queryString="";
+  } else if (hash==="#clima") {
+    file ="./pages/clima.html";
+    url ="clima";
+    queryString ="";
+  } else if (hash==="#polen") {
+    file ="./pages/polen.html";
+    url ="polen";
+    queryString ="";
   }
 
-  $.getJSON(
-    url,
-    function( response ){
-      // get json data and callback
-      callback(file, response);
-    }
-  );
+  $.ajax({
+      url: "http://airebahia.zz.mu/serviceCall.php?url="+ url +"&query="+ queryString,
+      type: 'GET',
+      //dataType: "json",
+      success: function( response ){
+        // get json data and callback
+        callback(file, response);
+      },
+      error: function(xhr, textStatus, errorThrown){
+        console.log(textStatus + ":" + errorThrown);
+      }
+  });
 
 }
 
